b362ae246e9892f1869850001c54e23f61a97c9f
chenshijie <chrischen369@outlook.com>
2017-12-15 00:03:04 +0800

    Include property files to get data source path and transfer to JDBC using sqlite.

--- src/quakeViewer/dataLoader.java
+++ src/quakeViewer/dataLoader.java
@@ -3,15 +3,20 @@ package quakeViewer;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.sql.*;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.Iterator;
-import java.sql.*;
 import java.util.Properties;
 
 public class dataLoader {
     public ArrayList<earthQuake> quakes = new ArrayList<>();
+
+    public dataLoader() {
+        loadData();
+        //printElement(quakes);
+        //System.out.println(quakes.size());
+    }
 
     public void printElement() {
         Iterator iter = quakes.iterator();
@@ -20,35 +25,30 @@ public class dataLoader {
         }
     }
 
-    public dataLoader () {
-        loadData();
-        //printElement(quakes);
-        //System.out.println(quakes.size());
-    }
     private void loadData() {
         //establish jdbc connection
         Connection connection = null;
-        ResultSet resultSet =null;
+        ResultSet resultSet = null;
         Statement statement = null;
         //load path to data source from .cnf file, if not, use default setting
         Properties defProp = new Properties();
-        defProp.put("path","");
+        defProp.put("path", "");
         Properties dbProp = new Properties(defProp);
         String path = System.getProperty("user.dir");
-        try(BufferedReader conf = new BufferedReader(new FileReader(path + "/src/preference.cnf"))){
+        try (BufferedReader conf = new BufferedReader(new FileReader(path + "/src/preference.cnf"))) {
             dbProp.load(conf);
-        }catch (IOException e) {
+        } catch (IOException e) {
             System.err.println("Using default path for data source");
         }
         //load data from sqlite database
-        try{
+        try {
             Class.forName("org.sqlite.JDBC");
             connection = DriverManager.getConnection("jdbc:sqlite:" + dbProp.getProperty("path"));
             statement = connection.createStatement();
             String q1 = "SELECT id, UTC_date, latitude, longitude, depth, magnitude, region, area_id FROM quakes";
             resultSet = statement.executeQuery(q1);
             resultSet.next();
-            while(resultSet.next()) {
+            while (resultSet.next()) {
                 int id = resultSet.getInt("id");
                 String date = resultSet.getString("UTC_date");
                 float latitude = resultSet.getFloat("latitude");
@@ -58,17 +58,17 @@ public class dataLoader {
                 String region = resultSet.getString("region");
                 int areaID = resultSet.getInt("area_id");
                 //store data into arrayList
-                earthQuake ek = new earthQuake(id,date,latitude,longitude,depth,magnitude,region,areaID);
+                earthQuake ek = new earthQuake(id, date, latitude, longitude, depth, magnitude, region, areaID);
                 quakes.add(ek);
             }
-        } catch (ClassNotFoundException e){
+        } catch (ClassNotFoundException e) {
             System.out.println(e.getMessage());
             e.printStackTrace();
         } catch (SQLException sqlE) {
             System.out.println(sqlE);
             sqlE.printStackTrace();
         } finally {
-            try{
+            try {
                 resultSet.close();
                 statement.close();
                 connection.close();
@@ -77,32 +77,44 @@ public class dataLoader {
             }
         }
     }
+
     static class idComparator implements Comparator {
         public int compare(Object o1, Object o2) {
             earthQuake e1 = (earthQuake) o1;
             earthQuake e2 = (earthQuake) o2;
-            return new Integer(e1.getId()).compareTo(new Integer(e2.getId()));
+            int int1 = ((earthQuake) o1).getId();
+            int int2 = ((earthQuake) o2).getId();
+            return int1<int2 ? 1:0;
         }
     }
+
     static class latitudeComparator implements Comparator {
         public int compare(Object o1, Object o2) {
             earthQuake e1 = (earthQuake) o1;
             earthQuake e2 = (earthQuake) o2;
-            return new Float(e1.getLatitude()).compareTo(new Float(e2.getLatitude()));
+            float f1 = ((earthQuake) o1).getLatitude();
+            float f2=  ((earthQuake) o2).getLatitude();
+            return f1 < f2 ? 1:0;
         }
     }
+
     static class longitudeComparator implements Comparator {
         public int compare(Object o1, Object o2) {
             earthQuake e1 = (earthQuake) o1;
             earthQuake e2 = (earthQuake) o2;
-            return new Float(e1.getLongitude()).compareTo(new Float(e2.getLongitude()));
+            float f1 = ((earthQuake) o1).getLongitude();
+            float f2 = ((earthQuake) o2).getLongitude();
+            return f1<f2 ? 1:0;
         }
     }
+
     static class depthComparator implements Comparator {
         public int compare(Object o1, Object o2) {
             earthQuake e1 = (earthQuake) o1;
             earthQuake e2 = (earthQuake) o2;
-            return new Float(e1.getDepth()).compareTo(new Float(e2.getDepth()));
+            float f1 = ((earthQuake) o1).getDepth();
+            float f2 = ((earthQuake) o2).getDepth();
+            return f1<f2 ? 1:0;
         }
     }
 
@@ -110,9 +122,12 @@ public class dataLoader {
         public int compare(Object o1, Object o2) {
             earthQuake e1 = (earthQuake) o1;
             earthQuake e2 = (earthQuake) o2;
-            return new Float(e1.getMagnitude()).compareTo(new Float(e2.getMagnitude()));
+            float f1 = ((earthQuake) o1).getMagnitude();
+            float f2 = ((earthQuake) o2).getMagnitude();
+            return f1<f2 ? 1:0;
         }
     }
+
     static class dateComparator implements Comparator {
         public int compare(Object o1, Object o2) {
             earthQuake e1 = (earthQuake) o1;
